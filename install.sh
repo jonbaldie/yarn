898124d90caedce157cb404eb642a6cf54d7df96
Jonathan Baldie <jon@jonbaldie.com>
2017-09-08 20:15:22 +0100

    the yarn installer already adds yarn to the path

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 apk update
 apk add nodejs curl bash gnupg
 
-touch $HOME/.profile && echo 'export PATH="$HOME/.yarn/bin:$PATH"' > $HOME/.profile
+touch $HOME/.profile
 
 curl -o- -L https://yarnpkg.com/install.sh | bash
 
